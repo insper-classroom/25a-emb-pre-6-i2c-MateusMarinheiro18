69b0fa719384a3f276768f33cd1cc2f1e0838c04
Mateus Marinheiro <mateusmcf@al.insper.edu.br>
2025-04-10 16:33:05 -0300

    feat ex2

--- exe2/main.c
+++ exe2/main.c
@@ -8,7 +8,9 @@
 
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
-#include "mpu6050.h"
+
+#define MPUREG_WHO_AM_I    0x75
+#define MPUREG_INT_ENABLE  0x38
 
 const int I2C_CHIP_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 20;
@@ -21,17 +23,18 @@ void i2c_task(void *p) {
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    uint8_t buffer[6];
+    uint8_t buffer[1];
+    uint8_t reg_address;
 
-    // read whoami
-    uint8_t reg_address = 0x75;
-    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
+    reg_address = MPUREG_WHO_AM_I;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true);
     i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
-    printf("WHOAMI: 0x%X \n", buffer[0]);
+    printf("WHO_AM_I: 0x%X\n", buffer[0]);
 
-    // TODO
-    // Leia o INT_ENABLE e imprima o valor
-    printf("INT_ENABLE: 0x%X \n", buffer[0]);
+    reg_address = MPUREG_INT_ENABLE;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true);
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
+    printf("INT_ENABLE: 0x%X\n", buffer[0]);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
@@ -40,7 +43,7 @@ void i2c_task(void *p) {
 
 int main() {
     stdio_init_all();
-    printf("Start RTOS \n");
+    printf("Start RTOS\n");
 
     xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
